de8b0a96afa8b3cec4e1fc7617a59b112bf0f672
cs20m039 <cs20m039@technikum-wien.at>
2024-04-07 15:49:06 +0200

    Code Refactoring

--- AnalyzingSignature/Signature_Read_Range.py
+++ AnalyzingSignature/Signature_Read_Range.py
@@ -14,7 +14,7 @@ logging.basicConfig(level=logging.INFO,
 # Configuration
 INTERVAL_START = 1
 INTERVAL_END = 400  # Adjusted according to the setup configuration
-READ_MODE = 'header'  # Options: 'header', 'footer', 'both'
+READ_MODE = 'footer'  # Options: 'header', 'footer', 'both'
 READ_LENGTH = INTERVAL_END  # Adjusted according to the setup configuration
 MALICIOUS_DIRECTORY = "/home/cs20m039/thesis/dataset1/malicious"
 BENIGN_DIRECTORY = "/home/cs20m039/thesis/dataset1/benign"
