8b7dcb854b6c68a363ea74f07379ca07dde9107d
cs20m039 <cs20m039@technikum-wien.at>
2024-04-08 13:21:41 +0200

    Code Refactoring

--- Analysing/Signature_Compare_Range.py
+++ Analysing/Signature_Compare_Range.py
@@ -3,8 +3,8 @@ import os
 import re
 from collections import defaultdict
 
-# Choose mode here: 'header-mode', 'footer-mode', 'both-mode'
-mode = 'both-mode'
+#both-mode, header-mode, footer-mode
+mode = 'both'
 
 print("Current Working Directory:", os.getcwd())
 
@@ -29,18 +29,18 @@ def find_max_index(csv_data, key_prefix):
 
 header_keys = []
 footer_keys = []
-if mode in ['header-mode', 'both-mode']:
+if mode in ['header', 'both']:
     max_header_index = max(find_max_index(malicious_csv, 'Header'), find_max_index(benign_csv, 'Header'))
     header_keys = [f'Header{i}' for i in range(1, max_header_index + 1)]
-if mode in ['footer-mode', 'both-mode']:
+if mode in ['footer', 'both']:
     max_footer_index = max(find_max_index(malicious_csv, 'Footer'), find_max_index(benign_csv, 'Footer'))
     footer_keys = [f'Footer{i}' for i in range(1, max_footer_index + 1)]
 
 matches = defaultdict(lambda: {"malicious": 0, "benign": 0, "files": {"malicious": [], "benign": []}})
 
 # Logic to compare headers and footers as per the chosen mode
-if mode in ['header-mode', 'footer-mode']:
-    keys_to_compare = header_keys if mode == 'header-mode' else footer_keys
+if mode in ['header', 'footer']:
+    keys_to_compare = header_keys if mode == 'headere' else footer_keys
     for key in keys_to_compare:
         header_footer_values = defaultdict(lambda: {"malicious": 0, "benign": 0, "files": {"malicious": [], "benign": []}})
         for row in malicious_csv:
@@ -58,7 +58,7 @@ if mode in ['header-mode', 'footer-mode']:
                 composite_key = (key, value_key)
                 matches[composite_key] = value_info
 
-elif mode == 'both-mode':
+elif mode == 'both':
     # Compare both headers and footers together for each row
     for row_m in malicious_csv:
         for row_b in benign_csv:
@@ -75,13 +75,13 @@ elif mode == 'both-mode':
                 matches[composite_key]["files"]["benign"].append(row_b["FileName"])
 
 # Reporting Matches
-if mode in ['header-mode', 'footer-mode']:
+if mode in ['header', 'footer']:
     for (column_key, match_value), info in matches.items():
-        mode_key = "Header" if mode == 'header-mode' else "Footer"
+        mode_key = "Header" if mode == 'header' else "Footer"
         print(f"MATCH: {mode_key} {column_key.split(mode_key)[-1]} Value = '{match_value}', "
               f"Occurrences in Malicious = {info['malicious']} ({', '.join(set(info['files']['malicious']))}), "
               f"Occurrences in Benign = {info['benign']} ({', '.join(set(info['files']['benign']))})")
-elif mode == 'both-mode':
+elif mode == 'both':
     for composite_keys, info in matches.items():
         for h_key, h_value, f_key, f_value in composite_keys:
             print(f"MATCH: {h_key} Value = '{h_value}', {f_key} Value = '{f_value}', "
